37f83cfe9aa145b1d945cfbcf6ace686059c06b4
Annabelle Peacock <aspeacock92@gmail.com>
2022-02-11 16:06:54 +0100

    needs css

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model');
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res) => {
+    Movie.find().then(allMovies => {
+        res.render('../views/movies/movies', {movies: allMovies});
+    }).catch(error => console.log(error))
+});
+// where do youdefine the movie id—it retrieves it from Mongoose?
+router.get('/movies/:movieId', (req, res) => {
+    const { movieId } = req.params;
+    Movie.findById(movieId)
+    .then(movie => {
+        res.render("../views/movies/movies-details", {movie})
+    }).catch(error => console.log(error))
+});
 
 module.exports = router;
